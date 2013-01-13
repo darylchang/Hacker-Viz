21bc93db131b95ae8c37b1cc5fcdf303cd030930
Daryl Chang <darylc123@gmail.com>
2013-01-13 04:02:12 -0800

    init

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python hacker.py
---
+++ hacker.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+import requests
+import json
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	"""r = requests.get("http://api.ihackernews.com/page")
+	r = r.json()
+	hacker_news_dict = {'name': 'hackernews', 'children':[]}
+	num = 0
+
+	for item in r["items"]:
+		if num>10:
+			break
+		else:
+			hacker_news_dict['children'].append({'points':item["points"],'link':item["url"], 'name':item["title"], 'comments':item["commentCount"]})
+ 			num = num+1
+
+	r = json.dumps(hacker_news_dict)
+	print hacker_news_dict, r
+
+	with open ('%s/static/flare.json' % os.path.abspath(os.path.dirname(__file__)),'w') as handle:
+		handle.write(r)"""
+
+	return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)

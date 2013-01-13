7fb47827d912364cf51583f02afb9d30dded7baa
Daryl Chang <darylc123@gmail.com>
2013-01-13 04:48:11 -0800

    init

--- hacker.py
+++ hacker.py
@@ -8,25 +8,26 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 	r = requests.get("http://api.ihackernews.com/page")
-	r = r.json()
-	hacker_news_dict = {'name': 'hackernews', 'children':[]}
-	num = 0
+	if (r):
+		r = r.json()
+		hacker_news_dict = {'name': 'hackernews', 'children':[]}
+		num = 0
 
-	for item in r["items"]:
-		if num>10:
-			break
-		else:
-			hacker_news_dict['children'].append({'points':item["points"],'link':item["url"], 'name':item["title"], 'comments':item["commentCount"]})
- 			num = num+1
+		for item in r["items"]:
+			if num>10:
+				break
+			else:
+				hacker_news_dict['children'].append({'points':item["points"],'link':item["url"], 'name':item["title"], 'comments':item["commentCount"]})
+	 			num = num+1
 
-	r = json.dumps(hacker_news_dict)
-	print hacker_news_dict, r
+		r = json.dumps(hacker_news_dict)
+		print hacker_news_dict, r
 
-	with open ('%s/static/flare.json' % os.path.abspath(os.path.dirname(__file__)),'w') as handle:
-		handle.write(r)
+		with open ('%s/static/flare.json' % os.path.abspath(os.path.dirname(__file__)),'w') as handle:
+			handle.write(r)
 
 	return render_template("index.html")
-	
+		
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
